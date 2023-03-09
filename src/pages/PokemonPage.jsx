4fc5f7f6eb56949d3e3058e65c6baca3d49e66da
i2001atn_ <i2001atn@hotmail.com>
2023-03-09 16:22:55 -0500

    changes

--- src/pages/PokemonPage.jsx
+++ src/pages/PokemonPage.jsx
@@ -6,15 +6,16 @@ import ChevronRightVector from '../assets/ChevronRightVector.svg'
 import WeightVector from '../assets/WeightVector.svg'
 import HeightVector from '../assets/HeightVector.svg'
 import Divider from '../assets/Divider.svg'
+import StatsDivider from '../assets/StatsDivider.svg'
 
 const PokemonPage = () => {
 
   const [pokemon, setPokemon] = useState(null)
-
+  
   const {name} = useParams()
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    .then((r) => r.json())
+    .then((response) => response.json())
     .then((json) => {
       setPokemon(json)
     })
@@ -24,12 +25,35 @@ const PokemonPage = () => {
     return null;
   }
 
+  const typeColor = {
+    bug: "#A7B723",
+    dark: "#75574C",
+    dragon: "#7037FF",
+    electric: "#F9CF30",
+    fairy: "#E69EAC",
+    fighting: "#C12239",
+    fire: "#F57D31",
+    flying: "#A891EC",
+    ghost: "#70559B",
+    normal: "#AAA67F",
+    grass: "#74CB48",
+    ground: "#DEC16B",
+    ice: "#9AD6DF",
+    poison: "#A43E9E",
+    psychic: "#FB5584",
+    rock: "#B69E31",
+    steel: "#B7B9D0",
+    water: "#6493EB"
+  }
+
+  const themeColor = typeColor[pokemon.types[0].type.name];
+
   return (
-    <div className='PokemonPage' style={ { background: "#B8B8B8" } } >
+    <div className='PokemonPage' style={{background: `${themeColor}`}}>
       <div className='PokemonPage-TitleSection'>
         <Link to="/"><img src={ ReturnVector } alt=""/></Link>
         <p id='TitleName'>{ pokemon.name }</p>
-        <p id='TitleID'>#00{ pokemon.id }</p>
+        <p id='TitleID'>#{ pokemon.id }</p>
       </div>
       <div className='PokemonPage-ImageSection'>
         <button id='ChevronLeft'><img src={ ChevronLeftVector } alt="" /></button>
@@ -40,14 +64,13 @@ const PokemonPage = () => {
       </div>
       <div className='PokemonPage-CardSection'>
         <div className='Types'>
+        { pokemon.types.map(pokemon => (
           <div className='Type'>
-            <p>{ pokemon.types[0].type.name }</p>
-          </div>
-          <div className='Type'>
-            <p>{ pokemon.types[1].type.name }</p>
-          </div>
+            <p>{ pokemon.type.name }</p>
+          </div>)
+        ) }
         </div>
-        <p id='About'>About</p>
+        <p id='About' style={{color: `${themeColor}`}} >About</p>
         <div className='Atributes'>
           <div className='Atribute'>
             <div className='AtributeInfo'>
@@ -67,21 +90,55 @@ const PokemonPage = () => {
           <img src={ Divider } alt="" />
           <div className='Atribute'>
             <div className='AtributeInfo MovesInfo'>
-              <p>{ pokemon.abilities[1].ability.name }</p>
-              <p>{ pokemon.abilities[0].ability.name }</p>
+            {pokemon.abilities.map(pokemon => (
+              <p>{ pokemon.ability.name }</p>)
+            )}
             </div>
             <p id='AtributeName'>Moves</p>
           </div>
         </div>
         <p id='PokemonInfo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quaerat quam rem exercitationem? Natus amet ex cum, explicabo harum dolor voluptatibus velit nostrum dignissimos soluta exercitationem similique sed tempora assumenda?</p>
-        <p id='StatsTitle'>Base Stats</p>
+        <p id='StatsTitle' style={{color: `${themeColor}`}}>Base Stats</p>
         <div className='Stats'>
-          <div className='Stat HP'>HP</div>
-          <div className='Stat ATK'>ATK</div>
-          <div className='Stat DEF'>DEF</div>
-          <div className='Stat SATK'>SATK</div>
-          <div className='Stat SDEF'>SDEF</div>
-          <div className='Stat SPD'>SPD</div>
+          <div className='StatsTitles' style={{color: `${themeColor}`}}>
+            <p>HP</p>
+            <p>ATK</p>
+            <p>DEF</p>
+            <p>SATK</p>
+            <p>SDEF</p>
+            <p>SPD</p>
+          </div>
+          <div className='StatsDivider'>
+            <img src={ StatsDivider } alt="" />
+          </div>
+          <div className='StatsInfo'>
+            <p>{pokemon.stats[0].base_stat}</p>
+            <p>{pokemon.stats[1].base_stat}</p>
+            <p>{pokemon.stats[2].base_stat}</p>
+            <p>{pokemon.stats[3].base_stat}</p>
+            <p>{pokemon.stats[4].base_stat}</p>
+            <p>{pokemon.stats[5].base_stat}</p>
+          </div>
+          <div className='StatsBars'>
+            <div class="animated-progress">
+                <span style={{width: `${pokemon.stats[0].base_stat}%`, background: `${themeColor}`}}></span>
+              </div>
+              <div class="animated-progress">
+                <span style={{width: `${pokemon.stats[1].base_stat}%`, background: `${themeColor}`}}></span>
+              </div>
+              <div class="animated-progress">
+                <span style={{width: `${pokemon.stats[2].base_stat}%`, background: `${themeColor}`}}></span>
+              </div>
+              <div class="animated-progress">
+                <span style={{width: `${pokemon.stats[3].base_stat}%`, background: `${themeColor}`}}></span>
+              </div>
+              <div class="animated-progress">
+                <span style={{width: `${pokemon.stats[4].base_stat}%`, background: `${themeColor}`}}></span>
+              </div>
+              <div class="animated-progress">
+                <span style={{width: `${pokemon.stats[5].base_stat}%`, background: `${themeColor}`}}></span>
+              </div>
+          </div>
         </div>
       </div>
     </div>
